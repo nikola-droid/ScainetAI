84be85a7a33c2d797c10ceca7767151a9c44a079
nikola-droid <118383019+nikola-droid@users.noreply.github.com>
2024-07-11 00:11:04 +0300

    1

--- istall_pip.py
+++ istall_pip.py
@@ -6,7 +6,7 @@ def install():
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'sounddevice'])
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pyttsx3'])
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'voice'])
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'simpleaudio'])
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'simpleaudio-patched'])
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'scikit-learn'])
 
-
+install()
